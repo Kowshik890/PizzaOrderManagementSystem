44fb484a89835e2028e5bb652af4e318c384238d
Kowshik890 <kavro.890@gmail.com>
2021-01-22 16:56:28 +0100

    Update Nav, Pizza, Supp, Ing page - '22.01'

---
+++ AMD-Project/index.php
@@ -0,0 +1,24 @@
+<?php require_once 'conn.php';?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <title>Home Page</title>
+    </head>
+<body>
+
+    <div>
+        <?php include_once 'nav.php'?>
+    </div>
+    <div class="container">
+        <h1 style="text-align: center">Welcome To Baker Page</h1>
+    </div>
+    
+
+</body>
+
+</html>
